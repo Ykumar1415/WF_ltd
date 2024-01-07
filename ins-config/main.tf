834c0f25aea376f47c275a9da5c03f8ad7892018
Yogesh Saini <yogeshsaini1213@gmail.com>
2024-01-07 05:36:00 +0530

    Configuration files for action

--- ins-config/main.tf
+++ ins-config/main.tf
@@ -133,6 +133,30 @@ resource "aws_security_group" "maingroup" {
       self             = false
       to_port          = 5000
     },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+      
+    }
   ]
 }
 
